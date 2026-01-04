6f49463299fb2490aceb1caa58fd5f9520357073
Eugene Romaniuk <zhenyarom08@gmail.com>
2026-01-04 17:09:45 +0100

    Initial project structure and data

---
+++ src/tv.py
@@ -0,0 +1,111 @@
+from tvDatafeed import TvDatafeed, Interval
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import t
+import datetime as dt
+
+
+def tv_download(tickers: list[tuple[str, str, str]], interval: str, n_bars: int) -> pd.DataFrame:
+    tv = TvDatafeed()
+    returns_df = pd.DataFrame()
+
+    for i in range(len(tickers)):
+        df: pd.DataFrame = tv.get_hist(symbol=tickers[i][0], exchange=tickers[i][1], interval=interval, n_bars=n_bars, fut_contract=1)
+        returns_df[f"{tickers[i][0]}"] = df["close"].values
+        returns_df.set_index(df.index.values, inplace=True)
+
+    return returns_df.dropna()
+
+
+def tv_import(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    df.set_index(df.iloc[:, 0])
+    df = df.drop(df.columns[0], axis=1)
+    return df
+        
+
+def get_pnl(price_df: pd.DataFrame, weights: list[float], investment: float) -> pd.Series:
+    simple_returns = price_df.pct_change().dropna() * investment
+    return simple_returns @ weights
+
+
+def get_risk_t(mu, std, days, ci, num_sims = 100_000, df = 5):
+    sim_pnl = t.rvs(df=df, size=(num_sims, days)) 
+    sim_pnl = sim_pnl * std / np.sqrt(df / (df - 2)) + mu
+
+    total_pnl = sim_pnl.sum(axis=1) #We are summing up simple returns, but that's correct in the case of PnL modelling
+    var = -np.quantile(total_pnl, 1 - ci) #Value at Risk -- our worst case loss in n days with a certain confidence
+    es = -total_pnl[total_pnl <= -var].mean() #Expected Shortfall -- mean losses beyond our VaR
+
+    return (float(var), float(es), pd.DataFrame(sim_pnl), pd.Series(total_pnl))
+\
+
+def get_risk(mu, std, days, ci, num_sims = 100_000, df = 5, get_sim_returns = False):
+    res = get_risk_t(mu, std, days, ci, num_sims, df)
+    if get_sim_returns:
+        return res
+    else:
+        return (res[0], res[1])
+
+
+def create_hist_plot(data: pd.Series, title: str, lines: tuple[float], lines_labels: tuple[str], line_colors: tuple[str]):
+    plt.figure(figsize=(10, 6))
+    plt.hist(data, bins=50, alpha=0.75, color="blue", edgecolor="black")
+
+    if lines != None:
+        for line, label, color in zip(lines, lines_labels, line_colors):
+            plt.axvline(-line, linestyle = "--", label = f"{label}: ${round(line):,}", color = color)
+
+    plt.title(title)
+    plt.xlabel("Returns")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.show()
+
+def to_simple(returns: float) -> float:
+    return np.exp(returns) - 1    
+
+if __name__ == "__main__":
+    tickers_list = [
+        ("CL", "NYMEX", "WTI Crude Oil"), 
+        ("RB", "NYMEX", "RBOB Gasoline"),
+        ("NG", "NYMEX", "Natural Gas"),
+        ("GC", "COMEX", "Gold"),
+        ("SI", "COMEX", "Silver"),
+        ("PL", "NYMEX", "Platinum"),
+        ("ALI", "COMEX", "Aluminum (NOT Aluminium)"),
+        ("HG", "COMEX", "Copper"),
+        ("LE", "CME", "Live Cattle"),
+        ("ZS", "CBOT", "Soybeans")
+    ]
+
+    df = tv_import("tv_data.csv")
+
+    weights = [1/len(tickers_list) for i in range(len(tickers_list))]
+    investment = 5_000_000_000
+    pnl: pd.Series = get_pnl(df, weights, investment)
+
+    VaR, ES, sim_pnl, total_pnl = get_risk(pnl.mean(), pnl.std(), 21, 0.91, get_sim_returns=True, df=4)
+
+    create_hist_plot(total_pnl, title="Risk Metrics over 21 days", lines=(VaR, ES), lines_labels=("VaR", "Expected Shortfall"), line_colors=("yellow", "red"))
+
+
+    '''
+    2008 Financial Crisis
+    2020 Oil Price Crash
+    2022 Energy Crisis
+    Increased Volatility
+    '''
+
+    '''
+    Scenario 1. 2008 Financial Crisis
+    '''
+    start_date = dt.datetime(2008, 9, 15)
+    end_date = dt.datetime(2008, 10, 10)
+    scenario_df = df[df.index >= start_date and df.index <= end_date]
+    scenario_pnl = get_pnl(scenario_df, weights, investment)
+
+    VaR, ES, sim_pnl, total_pnl = get_risk(scenario_pnl.mean(), scenario_pnl.std(), 30, df=4, get_sim_returns=True)
+
+    create_hist_plot(total_pnl, title="Risk Metrics over 30 days", lines=(VaR, ES), lines_labels=("VaR", "Expected Shortfall"), line_colors=("yellow", "red"))
