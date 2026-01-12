Replaced old t distribution computation with multivariate Student t distribution Fixed issue with historical equity charts Added Increased Tail Risk test

src/tv.py
@@ -2,7 +2,6 @@ from tvDatafeed import TvDatafeed, Interval
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import t
 import datetime as dt
 
 
@@ -35,27 +34,42 @@ def get_risk_hist(pnl: pd.Series, days, ci):
     var = -np.quantile(range_pnl, 1 - ci)
     es = -range_pnl[range_pnl <= -var].mean()
 
-    return (float(var), float(es), pd.Series(range_pnl))
+    return float(var), float(es), pd.Series(range_pnl)
+
+def get_risk_t(df, days, ci, investment, weights, num_sims = 100_000, nu = 4, volatility_scaler = 1):
+    
+    pnl = df.pct_change().dropna() * investment
+
+    mu = pnl.mean().values
+    Sigma = pnl.cov().values
+    n_assets = len(mu)
+    
+    shape = Sigma * (nu - 2) / nu
+    shape = shape * volatility_scaler ** 2
+    L = np.linalg.cholesky(shape)
+
+    #Generating all normal draws (our numerator)
+    rng = np.random.default_rng(None) #Creating the random seed
+    Z = rng.standard_normal(size=(num_sims, days, n_assets))
+    daily_normal = Z @ L.T #shape: (num_sims, days, n_assets)
+
+    #Generating 1 chi-square per simulation and scaling it (our denominator)
+    U = rng.chisquare(df=nu, size=num_sims) #shape: (num_sims,)
+    scale = np.sqrt(U / nu)
+    scale = scale[:, None, None] #shape: (num_sims, 1, 1)
+
+    #Combining them into multivariate Student t ( mu + Z/(sqrt(U/nu)) )
+    daily_pnl = mu + daily_normal / scale #shape: (num_sims, days, n_assets)
+    
+    portfolio_daily = np.einsum('ijk,k->ij', daily_pnl, weights) # daily_pnl @ weights -> (num_sims, days)
+    total_pnl = portfolio_daily.sum(axis=1) #shape: (num_sims,)
 
 
-def get_risk_t(mu, std, days, ci, num_sims = 100_000, df = 5):
-    sim_pnl = t.rvs(df=df, size=(num_sims, days)) 
-    sim_pnl = sim_pnl * std / np.sqrt(df / (df - 2)) + mu
+    var = -np.quantile(total_pnl, 1 - ci)
+    es = -total_pnl[total_pnl <= -var].mean()
 
-    total_pnl = sim_pnl.sum(axis=1) #We are summing up simple returns, but that's correct in the case of PnL modelling
-    var = -np.quantile(total_pnl, 1 - ci) #Value at Risk -- our worst case loss in n days with a certain confidence
-    es = -total_pnl[total_pnl <= -var].mean() #Expected Shortfall -- mean losses beyond our VaR
+    return float(var), float(es), pd.Series(total_pnl)
 
-    return (float(var), float(es), pd.DataFrame(sim_pnl), pd.Series(total_pnl))
-
-
-
-def get_risk(mu, std, days, ci, num_sims = 100_000, df = 5, get_sim_returns = False):
-    res = get_risk_t(mu, std, days, ci, num_sims, df)
-    if get_sim_returns:
-        return res
-    else:
-        return (res[0], res[1])
 
 
 def create_hist_plot(data: pd.Series, title: str, lines: tuple[float], lines_labels: tuple[str], line_colors: tuple[str]):
@@ -72,20 +86,21 @@ def create_hist_plot(data: pd.Series, title: str, lines: tuple[float], lines_lab
     plt.legend()
     plt.show()
 
-def pnl_chart(pnl, capital):
+def pnl_chart(pnl, capital, title):
     equity = capital + pnl.cumsum()
     plt.figure()
     plt.plot(equity.index, equity)
     plt.axhline(0)
-    plt.title("Portfolio Equity During 2020 Oil Crash Stress Scenario")
+    plt.title(title)
     plt.xlabel("Date")
     plt.ylabel("Equity ($)")
     plt.tight_layout()
     plt.show()
 
-def scenario_hist(start_date: dt.datetime, end_date: dt.datetime, title):
+def scenario_hist(df, weights, investment, start_date: dt.datetime, end_date: dt.datetime, title):
     days=5
     ci=0.95
+    nu = 5
 
     scenario_df = df[df.index.to_series().between(start_date, end_date)]
 
@@ -103,8 +118,8 @@ def scenario_hist(start_date: dt.datetime, end_date: dt.datetime, title):
 
     total_loss_gain = scenario_pnl.sum()
 
-    pnl_chart(scenario_pnl, 250_000_000)
-    VaR_mc, ES_mc, sim_pnl, total_pnl = get_risk(scenario_pnl.mean(), scenario_pnl.std(), days=days, ci=ci, df=4, get_sim_returns=True)
+    pnl_chart(scenario_pnl, 250_000_000, title=f"{title}. Portfolio Equity")
+    VaR_mc, ES_mc, total_pnl = get_risk_t(scenario_df, days=days, ci=ci, investment=investment, weights=weights, nu=nu)
 
 
     create_hist_plot(total_pnl, title=f"{title} Monte Carlo Simulation over {days} days", lines=(VaR_mc, ES_mc), lines_labels=("VaR", "Expected Shortfall"), line_colors=("yellow", "red"))
@@ -127,7 +142,8 @@ def scenario_hist(start_date: dt.datetime, end_date: dt.datetime, title):
                 5-day ES (Student t): ${ES_mc:,.0f}
         """)
 
-if __name__ == "__main__":
+
+def main():
     tickers_list = [
         ("CL", "NYMEX", "WTI Crude Oil"), 
         ("RB", "NYMEX", "RBOB Gasoline"),
@@ -150,7 +166,7 @@ if __name__ == "__main__":
     start_date = dt.datetime(2020, 2, 20)
     end_date = dt.datetime(2020, 4, 30)
     
-    scenario_hist(start_date, end_date, "Stress Test Scenario 1. Oil Price Crash 2020")
+    scenario_hist(df, weights, investment, start_date, end_date, "Stress Test Scenario 1. Oil Price Crash 2020")
     
 
 #   Scenario 2. Energy Crisis 2022
@@ -158,22 +174,20 @@ if __name__ == "__main__":
     start_date = dt.datetime(2022, 2, 24)
     end_date = dt.datetime(2022, 10, 31)
     
-    scenario_hist(start_date, end_date, "Stress Test Scenario 2. Energy Crisis 2022")
+    scenario_hist(df, weights, investment, start_date, end_date, "Stress Test Scenario 2. Energy Crisis 2022")
 
 #   Scenario 3. Increased Volatility
     ci = 0.95
-
-    pnl = get_pnl(df, weights, investment)
-
+    nu = 5
     volatility_coefficient = 2
 
-    VaR_5, ES_5, sim_pnl_5, total_pnl_5 = get_risk(pnl.mean(), pnl.std() * volatility_coefficient, days=5, ci=ci, df=4, get_sim_returns=True)
+    VaR_5, ES_5, total_pnl_5 = get_risk_t(df, days=5, ci=ci, investment=investment, weights=weights, nu=nu, volatility_scaler=volatility_coefficient)
 
     create_hist_plot(total_pnl_5, title=f"Stress Test 3. Monte Carlo Simulation over {5} days", lines=(VaR_5, ES_5),
                     lines_labels=("VaR", "Expected Shortfall"), line_colors=("yellow", "red"))
 
 
-    VaR_10, ES_10, sim_pnl_10, total_pnl_10 = get_risk(pnl.mean(), pnl.std() * volatility_coefficient, days=10, ci=ci, df=4, get_sim_returns=True)
+    VaR_10, ES_10, total_pnl_10 = get_risk_t(df, days=10, ci=ci, investment=investment, weights=weights, nu=nu, volatility_scaler=volatility_coefficient)
 
     create_hist_plot(total_pnl_10, title=f"Stress Test 3. Monte Carlo Simulation over {10} days", lines=(VaR_10, ES_10), 
                     lines_labels=("VaR", "Expected Shortfall"), line_colors=("yellow", "red"))
@@ -183,9 +197,42 @@ if __name__ == "__main__":
         ---Stress Test Scenario 3. Increased Volatility---\n
             Parameters:\n
                 Volatility increase: {volatility_coefficient*100}%\n
+                Degrees of freedom: {nu}\n
             Results:\n
                 5-day VaR (Student t): ${VaR_5:,.0f}\n
                 5-day ES (Student t): ${ES_5:,.0f}\n
                 10-day VaR (Student t): ${VaR_10:,.0f}\n
                 10-day ES (Student t): ${ES_10:,.0f}
         """)
+
+# Scenario 4. Increased Tail Risk
+    ci = 0.95
+    nu = 3
+
+    VaR_5, ES_5, total_pnl_5 = get_risk_t(df, days=5, ci=ci, investment=investment, weights=weights, nu=nu)
+
+    create_hist_plot(total_pnl_5, title=f"Stress Test 4. Monte Carlo Simulation over {5} days", lines=(VaR_5, ES_5),
+                    lines_labels=("VaR", "Expected Shortfall"), line_colors=("yellow", "red"))
+
+
+    VaR_10, ES_10, total_pnl_10 = get_risk_t(df, days=10, ci=ci, investment=investment, weights=weights, nu=nu)
+
+    create_hist_plot(total_pnl_10, title=f"Stress Test 4. Monte Carlo Simulation over {10} days", lines=(VaR_10, ES_10), 
+                    lines_labels=("VaR", "Expected Shortfall"), line_colors=("yellow", "red"))
+
+
+    print(f"""
+        ---Stress Test Scenario 4. Increased Tail Risk---\n
+            Parameters:\n
+                Degrees of freedom: {nu}\n
+            Results:\n
+                5-day VaR (Student t): ${VaR_5:,.0f}\n
+                5-day ES (Student t): ${ES_5:,.0f}\n
+                10-day VaR (Student t): ${VaR_10:,.0f}\n
+                10-day ES (Student t): ${ES_10:,.0f}
+        """)
+
+
+if __name__ == "__main__":
+    main()
+